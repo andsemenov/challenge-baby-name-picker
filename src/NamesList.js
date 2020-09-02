f6662d7477ae0cd94dd84367f3787d6f17eef106
Andrei <andsemenov@gmail.com>
2020-09-02 18:09:24 +0100

    Added toLowerCase to searchbar

--- src/NamesList.js
+++ src/NamesList.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import babyNames from "./babyNamesData.json";
 
 function NamesList(props) {
   let searched = babyNames.slice(0);
+  let [nameClicked, setNameClicked] = useState("");
   if (props.searchString.length !== 0) {
     searched = babyNames.filter((nameInfo) =>
-      nameInfo.name.startsWith(props.searchString)
+      nameInfo.name.toLowerCase().startsWith(props.searchString.toLowerCase())
     );
   }
-
+  function handleClick(event) {
+    setNameClicked(event.target);
+  }
+  console.log(nameClicked);
   return (
     <div className="namelist">
       {SortNames(searched).map((names, index) => (
-        <span key={index} className={"name " + names.sex}>
+        <span key={index} className={"name " + names.sex} onClick={handleClick}>
           {names.name}
         </span>
       ))}
